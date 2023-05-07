21f5a1cb4ecce31a2baf63a340c0dcf332016ffc
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-05-07 09:49:16 -0400

    change s for input optimize removing one line

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -4,23 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static String decodeString(String s) {
+    public static String decodeString(String input) {
         Pattern pattern = Pattern.compile("(\\d+)\\[([a-zA-Z]*)\\]");
-        Matcher matcher = pattern.matcher(s);
+        Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
             int count = Integer.parseInt(matcher.group(1));
             String letters = matcher.group(2);
-            s = s.replace(matcher.group(), letters.repeat(count));
-            matcher.reset(s);
+            input = input.replace(matcher.group(), letters.repeat(count));
+            matcher.reset(input);
         }
 
-        return s;
+        return input;
     }
 
     public static void main(String[] args) {
-        String s = "3[a2[b]]";
-        String decoded = decodeString(s);
-        System.out.println(decoded); // Output: abbabbabb
+        String input = "3[a2[b]]";
+        System.out.println(decodeString(input)); // Output: abbabbabb
     }
 }
